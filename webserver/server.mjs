9c8a706fc0daa540aa9720b073801a188c6d896e
Fabio Baser <f.baser@monday-consulting.com>
2023-05-16 15:42:37 +0200

    adds mongodb-connection

--- webserver/server.mjs
+++ webserver/server.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import chalk from "chalk";
+import { MongoClient } from "mongodb";
 
 import usersRoutes from "./routes/users.mjs";
 import postsRoutes from "./routes/posts.mjs";
@@ -7,9 +8,23 @@ import postsRoutes from "./routes/posts.mjs";
 const server = express();
 server.use(express.json());
 
+const dbClient = new MongoClient(
+  "mongodb+srv://admin:admin@datenbank1.ozyag9i.mongodb.net/"
+);
+await dbClient.connect();
+const db = dbClient.db("webapp");
+
 usersRoutes(server);
 postsRoutes(server);
 
+async function testDB() {
+  const collection = db.collection("users");
+  const users = await collection.find({}).toArray();
+  console.log(users);
+}
+
 server.listen(3000, () => {
-    console.log(chalk.green("Server is now running on port 3000"));
+  console.log(chalk.green("Server is now running on port 3000"));
+
+  testDB();
 });
